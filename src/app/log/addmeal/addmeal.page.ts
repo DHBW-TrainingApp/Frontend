7cce955cbc73c7bf378cc60a730a487fb67151a3
0Raspbinary1 <rgremsperger@googlemail.com>
2022-04-26 23:20:52 +0200

    prepare and changing code for tests

--- src/app/log/addmeal/addmeal.page.ts
+++ src/app/log/addmeal/addmeal.page.ts
@@ -4,6 +4,7 @@ import { jsonEval } from '@firebase/util';
 import { CrudService } from './../../services/crud.service';
 import { FormGroup, FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
+import { stringify } from 'querystring';
 
 @Component({
   selector: 'app-addmeal',
@@ -12,6 +13,15 @@ import { Router } from '@angular/router';
 })
 export class AddmealPage implements OnInit {
   mealForm: FormGroup;
+  filledForm: boolean = true;
+  bProtein: boolean = true;
+  bSugar: boolean = true;
+  bSatFat: boolean = true;
+  bFat: boolean = true;
+  bCarbs: boolean = true;
+  bEnergy: boolean = true;
+  bTitle: boolean = true;
+
 
   constructor(
     private crudService: CrudService,
@@ -31,7 +41,73 @@ export class AddmealPage implements OnInit {
     });
   }
 
+
+
   onSubmit() {
+    //let data = stringify(this.mealForm.value);
+    //let protein = this.mealForm.get('protein').value;
+    //console.log("protein wert ist: " + protein);
+    console.log("onSubmit() begin  checking all fields whether 'null'");
+
+    if( this.mealForm.get('title').value == '') {
+      this.bTitle = false;
+    }      console.log("title: " + this.bTitle);
+
+    if(this.mealForm.get('energy').value == '') {
+      this.bEnergy = false;
+    }      console.log("energy: " + this.bEnergy);
+
+    if( this.mealForm.get('carbs').value == '') {
+      this.bCarbs = false;
+    }      console.log("carbs: " + this.bCarbs);
+
+    if( this.mealForm.get('fat').value == '') {
+      this.bFat = false;
+    }      console.log("fat: " + this.bFat);
+
+    if( this.mealForm.get('satFat').value == '') {
+      this.bSatFat = false;
+    }      console.log("satFat: " + this.bSatFat);
+
+    if( this.mealForm.get('sugar').value == '') {
+      this.bSugar = false;
+    }      console.log("sugar: " + this.bSugar);
+
+    if( this.mealForm.get('protein').value == '') {
+      this.bProtein = false;
+    }      console.log("protein: " + this.bProtein);
+
+    console.log("protein wert ist: " + this.mealForm.get('protein').value);
+
+    //-----------------------------------------------------------------
+    console.log("if one of the fields is null, filledForm should be false: ");
+    if(!this.bTitle || !this.bEnergy || !this.bCarbs|| !this.bFat || !this.bSatFat || !this.bSugar || !this.bProtein){
+          this.filledForm = false;
+    } else {
+      this.filledForm = true;
+    }
+    console.log("filledForm is: " + this.filledForm);
+
+    if(this.filledForm == false){
+      return false;
+
+    /* if (!this.mealForm.valid) {
+       return false;*/
+    } else {
+        this.crudService
+          .createMeal(this.mealForm.value)
+          .then(() => {
+            this.mealForm.reset();
+            this.router.navigate(['/todo-list']);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }
+      console.log("onSubmit() ende");
+  }
+
+  /*   onSubmit() {
     if (!this.mealForm.valid) {
       return false;
     } else {
@@ -44,6 +120,5 @@ export class AddmealPage implements OnInit {
         .catch((err) => {
           console.log(err);
         });
-    }
-  }
+    } */
 }
